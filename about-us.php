d76f27346132be3e287d1f09b05d10314a619de6
Sinan Özçelik <sinanozcelik@yaani.com>
2024-02-13 11:34:54 +0300

    Add files via upload

---
+++ about-us.php
@@ -0,0 +1,246 @@
+<?php include 'header.php'; ?>
+<!-- TOP IMAGE -->
+<section class="topSingleBkg topPageBkg">
+	<div class="item-img top-blog-list"></div>
+	<div class="inner-desc">
+		<h1 class="post-title single-post-title">About Us</h1>
+		<span class="post-subtitle">Experienced Professionals</span>
+	</div>
+</section>
+<!-- /TOP IMAGE -->
+<!-- WRAP CONTENT -->
+<div id="wrap-content" class="page-content custom-page-template">
+	<!-- SECTION 1 -->
+	<div id="home-content-1" class="home-section home-section-1">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-4 mobile-margin-b48">
+					<div class="icon-holder2 alignc">
+						<i class="far fa-star"></i>
+						<div class="widgettitle icon-title">Famous Chefs</div>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec
+							ullamcorper mattis.</p>
+					</div>
+				</div>
+				<!-- /col-md-4 -->
+				<div class="col-md-4 mobile-margin-b48">
+					<div class="icon-holder2 alignc">
+						<i class="fas fa-seedling"></i>
+						<div class="widgettitle icon-title">Exotic Flavors</div>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec
+							ullamcorper mattis.</p>
+					</div>
+				</div>
+				<!-- /col-md-4 -->
+				<div class="col-md-4">
+					<div class="icon-holder2 alignc">
+						<i class="far fa-lemon"></i>
+						<div class="widgettitle icon-title">Vegetable Juices</div>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec
+							ullamcorper mattis.</p>
+					</div>
+				</div>
+				<!-- /col-md-4 -->
+			</div>
+			<!-- /row -->
+		</div>
+		<!-- /container -->
+	</div>
+	<!-- /SECTION 1 -->
+	<!-- SECTION 2 -->
+	<div id="about-us-2" class="home-section">
+		<div class="container">
+			<div class="row margin-b72">
+				<div class="col-md-12">
+					<div class="alignc">
+						<div class="smalltitle margin-b16">Concept</div>
+						<h2 class="home-title">Culinary Vision</h2>
+						<div class="width60">For a truly memorable dining experience, cuisine and atmosphere are paired
+							as thoughtfully as food and wine. Ut enim ad minim veniam, quis nostrud exercitation
+							ullamco.</div>
+					</div>
+				</div>
+				<!-- /col-md-12 -->
+			</div>
+			<!-- /row -->
+			<div class="row">
+				<div class="col-md-6 mobile-margin-b24">
+					<img class="img-fluid" src="images/home/home-5.jpg" alt="">
+				</div>
+				<!-- /col-md-6 -->
+				<div class="col-md-6">
+					<img class="img-fluid" src="images/home/home7.jpg" alt="">
+				</div>
+				<!-- /col-md-6 -->
+			</div>
+			<!-- /row -->
+		</div>
+		<!-- /container -->
+	</div>
+	<!-- /SECTION 2 -->
+	<!-- SECTION 3 -->
+	<div id="about-us-3" class="home-section about-us-3">
+		<div class="container">
+			<div class="row margin-b72">
+				<div class="col-md-6">
+					<div class="smalltitle margin-b16">Team</div>
+					<h2 class="home-title">Great Chef’s</h2>
+				</div>
+				<!-- /col-md-6 -->
+				<div class="col-md-6">
+					<p>Id pri probo nonumy tractatos, illum deserunt qui no, eum at torquatos philosophia. Postea causae
+						delectus vim ei, has insolens phaedrum ut, has id latine laboramus. An cum adipiscing
+						conclusionemque, summo blandit eum id.</p>
+					<p>Vel ad expetendis comprehensam, eu cum senserit vituperatoribus. Lorem ipsum dolor sit amet,
+						consectetur adipiscing elit. Ut elit tellus, luctus.</p>
+				</div>
+				<!-- /col-md-6 -->
+			</div>
+			<!-- /row -->
+			<div class="row">
+				<div class="col-sm-6 col-md-3">
+					<div class="team-member">
+						<div class="team-img">
+							<img src="images/team/team-1.jpg" class="img-fluid" alt="">
+						</div>
+						<div class="team-desc">
+							<h3 class="team-title">Anna Smith</h3>
+							<div class="smalltitle">Chef</div>
+							<p>Totam rem aperiam. Et iusto odio digniss ducimus qui blandi praesentium voluptatum
+								deleniti atque. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet,
+								consectetur.</p>
+						</div>
+					</div>
+				</div>
+				<!-- /col-md-3 -->
+				<div class="col-sm-6 col-md-3">
+					<div class="team-member">
+						<div class="team-img">
+							<img src="images/team/team-2.jpg" class="img-fluid" alt="">
+						</div>
+						<div class="team-desc">
+							<h3 class="team-title">Michel Carter</h3>
+							<div class="smalltitle">Manager</div>
+							<p>Totam rem aperiam. Et iusto odio digniss ducimus qui blandi praesentium voluptatum
+								deleniti atque. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet,
+								consectetur.</p>
+						</div>
+					</div>
+				</div>
+				<!-- /col-md-3 -->
+				<div class="col-sm-6 col-md-3">
+					<div class="team-member">
+						<div class="team-img">
+							<img src="images/team/team-3.jpg" class="img-fluid" alt="">
+						</div>
+						<div class="team-desc">
+							<h3 class="team-title">Sara Burton</h3>
+							<div class="smalltitle">Finance</div>
+							<p>Totam rem aperiam. Et iusto odio digniss ducimus qui blandi praesentium voluptatum
+								deleniti atque. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet,
+								consectetur.</p>
+						</div>
+					</div>
+				</div>
+				<!-- /col-md-3 -->
+				<div class="col-sm-6 col-md-3">
+					<div class="team-member">
+						<div class="team-img">
+							<img src="images/team/team-4.jpg" class="img-fluid" alt="">
+						</div>
+						<div class="team-desc">
+							<h3 class="team-title">Tyler Hart</h3>
+							<div class="smalltitle">Marketing</div>
+							<p>Totam rem aperiam. Et iusto odio digniss ducimus qui blandi praesentium voluptatum
+								deleniti atque. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet,
+								consectetur.</p>
+						</div>
+					</div>
+				</div>
+				<!-- /col-md-3 -->
+			</div>
+		</div>
+		<!-- /container -->
+	</div>
+	<!-- /SECTION 3 -->
+	<!-- SECTION 4 -->
+	<div id="home-content-6" class="home-section home-section-6 parallax padding-tb108">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-10 offset-md-1">
+					<div class="alignc">
+						<div class="smalltitle white margin-b16">Testimonials</div>
+						<h2 class="home-title">What Clients Say</h2>
+					</div>
+					<div class="owl-carousel owl-theme testimonial-slider">
+						<div class="testimonial-info">
+							<h4 class="testimonial-desc">"Caverta is a special place tiam sit amet orci eget eros
+								faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis
+								magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc vitae. Et iusto
+								odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti"</h4>
+							<div>Jerry Griffin</div>
+							<span>Griff Inc</span>
+						</div>
+						<div class="testimonial-info">
+							<h4 class="testimonial-desc">"In enim justo, rhoncus ut, imperdiet a, venenatis vitae,
+								justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus.
+								Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula,
+								porttitor eu, consequat. Duis leo. Sed fringilla mauris sit amet nibh."</h4>
+							<div>Laura Foster</div>
+							<span>Agro</span>
+						</div>
+						<div class="testimonial-info">
+							<h4 class="testimonial-desc">"Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel
+								augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas
+								tempus, tellus eget condimentum rhoncus, sem quam semper libero. officiis debitis aut
+								rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint."</h4>
+							<div>Angela Ray</div>
+							<span>Rays</span>
+						</div>
+						<div class="testimonial-info">
+							<h4 class="testimonial-desc">"Donec quam felis, ultricies nec, pellentesque eu, pretium
+								quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet
+								nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae,
+								justo. Itaque earum rerum hic tenetur a sapiente delectus. Accusantium doloremque."</h4>
+							<div>Marie Smith</div>
+							<span>The Smiths</span>
+						</div>
+					</div>
+					<!-- /testimonial-slider -->
+				</div>
+				<!-- /col-md-12 -->
+			</div>
+			<!-- /row -->
+		</div>
+		<!-- /container -->
+	</div>
+	<!-- /SECTION 4 -->
+	<!-- SECTION 5 -->
+	<div id="about-us-5" class="home-section">
+		<div class="container">
+			<div class="row align-items-center">
+				<div class="col-md-6 mobile-margin-b48">
+					<img class="img-fluid" src="images/home/home4.jpg" alt="" />
+				</div>
+				<!-- /col-md-6 -->
+				<div class="col-md-6">
+					<div class="alignc">
+						<div class="smalltitle margin-b16">Open Daily</div>
+						<h2 class="home-title">Come and Discover Our Restaurant</h2>
+						<p>For a truly memorable dining experience, cuisine and atmosphere are paired as thoughtfully as
+							food and wine. Ut enim ad minim veniam, quis nostrud exercitation ullamco. Quia consequuntur
+							magni dolores eos qui ratione voluptatem sequi nesciunt. Animi, id est laborum et dolorum
+							fuga.</p>
+						<p><a class="view-more margin-t24" href="contact.php">Contact Us</a></p>
+					</div>
+				</div>
+				<!-- /col-md-6 -->
+			</div>
+		</div>
+		<!-- /container -->
+	</div>
+	<!-- /SECTION 5 -->
+</div>
+<!-- /WRAP CONTENT -->
+
+<?php include 'footer.php'; ?>
---
+++ menu.php
@@ -0,0 +1,203 @@
+<?php include 'header.php'; ?>
+<!-- TOP IMAGE -->
+<section class="topSingleBkg topPageBkg">
+   <div class="item-img top-menu-3col"></div>
+   <div class="inner-desc">
+      <h1 class="post-title single-post-title">Menu</h1>
+      <span class="post-subtitle">Come and taste our delicacies</span>
+   </div>
+</section>
+<!-- /TOP IMAGE -->
+<!-- WRAP CONTENT -->
+<div id="wrap-content" class="page-content custom-page-template">
+   <div class="container">
+      <div class="row">
+         <div class="col-md-12">
+            <div class="page-holder custom-page-template">
+               <div class="row">
+                  <div class="col-md-12">
+                     <div class="alignc">
+                        <div class="smalltitle margin-b16">Discover</div>
+                        <h2 class="home-title">Menu a la Carte</h2>
+                        <div class="width60">Explore texture, color and of course the ultimate tastes with our menu of
+                           the season. All the ingredients are fresh and carefully selected by our chefs. Enjoy an
+                           extraordinary dinning experience.</div>
+                     </div>
+                     <!-- MENU TAB -->
+                     <ul class="nav nav-tabs menuTab" id="menuTab" role="tablist">
+                        <li class="nav-item">
+                           <a class="nav-link active" id="starters-tab" data-toggle="tab" href="#starters" role="tab"
+                              aria-controls="starters" aria-selected="true">STARTERS</a>
+                        </li>
+                        <li class="nav-item">
+                           <a class="nav-link" id="main-course-tab" data-toggle="tab" href="#main-course" role="tab"
+                              aria-controls="main-course" aria-selected="false">MAIN COURSE</a>
+                        </li>
+                        <li class="nav-item">
+                           <a class="nav-link" id="soups-tab" data-toggle="tab" href="#soups" role="tab"
+                              aria-controls="soups" aria-selected="false">SOUPS</a>
+                        </li>
+                        <li class="nav-item">
+                           <a class="nav-link" id="desserts-tab" data-toggle="tab" href="#desserts" role="tab"
+                              aria-controls="desserts" aria-selected="false">DESSERTS</a>
+                        </li>
+                     </ul>
+                     <div class="tab-content" id="menuTabContent">
+                        <!-- STARTERS -->
+                        <div class="tab-pane fade show active" id="starters" role="tabpanel"
+                           aria-labelledby="starters-tab">
+                           <ul class="food-menu menu-3cols">
+                              <li>
+                                 <h4><span class="menu-title">Tomato Bruschetta</span><span
+                                       class="menu-price">$4.00</span></h4>
+                                 <div class="menu-text">Tomatoes, Olive Oil, Cheese</div>
+                              </li>
+                              <li>
+                                 <h4><span class="menu-title">Avocado &amp; Mango Salsa</span><span
+                                       class="menu-price">$5.00</span></h4>
+                                 <div class="menu-text">Avocado, Mango, Tomatoes</div>
+                              </li>
+                              <li>
+                                 <h4><span class="menu-title">Marinated Grilled Shrimp</span><span
+                                       class="menu-price">$7.00</span></h4>
+                                 <div class="menu-text">Fresh Shrimp, Oive Oil, Tomato Sauce</div>
+                              </li>
+                              <li>
+                                 <h4><span class="menu-title">Baked Potato Skins</span><span
+                                       class="menu-price">$9.00</span></h4>
+                                 <div class="menu-text">Potatoes, Oil, Garlic</div>
+                              </li>
+                              <li>
+                                 <h4><span class="menu-title">Maitake Mushroom</span><span
+                                       class="menu-price">$10.00</span></h4>
+                                 <div class="menu-text">Whipped Miso, Yaki Sauce, Sesame</div>
+                              </li>
+                              <li>
+                                 <h4><span class="menu-title">Lobster Picante</span><span
+                                       class="menu-price">$12.00</span></h4>
+                                 <div class="menu-text">Grilled Corn Elote, Queso Cotija, Chili</div>
+                              </li>
+                              <li>
+                                 <h4><span class="menu-title">Jambon Iberico</span><span
+                                       class="menu-price">$10.00</span></h4>
+                                 <div class="menu-text">Smoked Tomato Aioli, Idizabal Cheese, Spiced Pine Nuts</div>
+                              </li>
+                              <li>
+                                 <h4><span class="menu-title">Garlic Baked Cheese</span><span
+                                       class="menu-price">$12.00</span></h4>
+                                 <div class="menu-text">Finnish Squeaky Cheese, Eggplant Conserva, Black Pepper</div>
+                              </li>
+                           </ul>
+                        </div>
+                        <!-- MAIN COURSE -->
+                        <div class="tab-pane fade" id="main-course" role="tabpanel" aria-labelledby="main-course-tab">
+                           <ul class="food-menu menu-3cols">
+                              <li>
+                                 <h4><span class="menu-title">Braised Pork Chops</span><span
+                                       class="menu-price">$21.00</span></h4>
+                                 <div class="menu-text">4 bone-in pork chops, olive oil, garlic, onion </div>
+                              </li>
+                              <li>
+                                 <h4><span class="menu-title">Coconut Fried Chicken </span><span
+                                       class="menu-price">$19.00</span></h4>
+                                 <div class="menu-text">8 chicken pieces, coconut milk, oil </div>
+                              </li>
+                              <li>
+                                 <h4><span class="menu-title">Chicken with Garlic &amp; Tomatoes </span><span
+                                       class="menu-price">$15.00</span></h4>
+                                 <div class="menu-text">Chicken, cherry tomatoes, olive oil, dry white wine</div>
+                              </li>
+                              <li>
+                                 <h4><span class="menu-title">Prime Rib</span><span class="menu-price">$25.00</span>
+                                 </h4>
+                                 <div class="menu-text">Rib, rosemary, black pepper, red wine </div>
+                              </li>
+                              <li>
+                                 <h4><span class="menu-title">Sriracha Beef Skewers</span><span
+                                       class="menu-price">$18.00</span></h4>
+                                 <div class="menu-text">Beef, garlic, sesame oil, vinegar </div>
+                              </li>
+                              <li>
+                                 <h4><span class="menu-title">Crispy Tuna Fregola</span><span
+                                       class="menu-price">$22.00</span></h4>
+                                 <div class="menu-text">Fregola, Baby Arugula Roasted, Green Olives, Pine Nuts</div>
+                              </li>
+                              <li>
+                                 <h4><span class="menu-title">Charred Lamb Ribs</span><span
+                                       class="menu-price">$20.00</span></h4>
+                                 <div class="menu-text">Za’atar, Turkish BBQ, Sesame Yoghurt</div>
+                              </li>
+                           </ul>
+                        </div>
+                        <!-- SOUPS -->
+                        <div class="tab-pane fade" id="soups" role="tabpanel" aria-labelledby="soups-tab">
+                           <ul class="food-menu menu-3cols">
+                              <li>
+                                 <h4><span class="menu-title">Terrific Turkey Chili</span><span
+                                       class="menu-price">$10.00</span></h4>
+                                 <div class="menu-text">Turkey, oregano, tomato paste, peppers </div>
+                              </li>
+                              <li>
+                                 <h4><span class="menu-title">Cream of Asparagus Soup</span><span
+                                       class="menu-price">$12.00</span></h4>
+                                 <div class="menu-text">Asparagus, potato, celery, onion, pepper </div>
+                              </li>
+                              <li>
+                                 <h4><span class="menu-title">Creamy Chicken &amp; Wild Rice Soup</span><span
+                                       class="menu-price">$9.00</span></h4>
+                                 <div class="menu-text">Cooked chicken, salt, butter, heavy cream </div>
+                              </li>
+                              <li>
+                                 <h4><span class="menu-title">Italian Sausage Tortellini</span><span
+                                       class="menu-price">$8.00</span></h4>
+                                 <div class="menu-text">Cheese tortellini, sausage, garlic, carrots, zucchini</div>
+                              </li>
+                              <li>
+                                 <h4><span class="menu-title">Italian Sausage Soup</span><span
+                                       class="menu-price">$10.00</span></h4>
+                                 <div class="menu-text">Italian sausage, garlic, carrots, zucchini </div>
+                              </li>
+                              <li>
+                                 <h4><span class="menu-title">Ham and Potato Soup</span><span
+                                       class="menu-price">$11.00</span></h4>
+                                 <div class="menu-text">Potatoes, ham, celery, onion, milk </div>
+                              </li>
+                           </ul>
+                        </div>
+                        <!-- DESSERTS -->
+                        <div class="tab-pane fade" id="desserts" role="tabpanel" aria-labelledby="desserts-tab">
+                           <ul class="food-menu menu-3cols">
+                              <li>
+                                 <h4><span class="menu-title">Summer Berry and Coconut Tart</span><span
+                                       class="menu-price">$10.00</span></h4>
+                                 <div class="menu-text">Raspberries, blackberries, blueberries, graham cracker</div>
+                              </li>
+                              <li>
+                                 <h4><span class="menu-title">Double Chocolate Cupcakes</span><span
+                                       class="menu-price">$12.00</span></h4>
+                                 <div class="menu-text">Chocolate, eggs, vanilla, milk </div>
+                              </li>
+                              <li>
+                                 <h4><span class="menu-title">Pumpkin Cookies Cream Cheese</span><span
+                                       class="menu-price">$10.00</span></h4>
+                                 <div class="menu-text">Pumpkin, sugar, butter, eggs </div>
+                              </li>
+                           </ul>
+                        </div>
+                     </div>
+                     <!-- /MENU TAB -->
+                  </div>
+                  <!-- /col-md-12 -->
+               </div>
+               <!-- /row -->
+            </div>
+            <!-- /custom-page-template -->
+         </div>
+         <!-- /col-md-12 -->
+      </div>
+      <!-- /row -->
+   </div>
+   <!-- /container -->
+</div>
+<!-- /WRAP CONTENT -->
+<?php include 'footer.php'; ?>
